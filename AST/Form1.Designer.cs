58e896587d0e4a30f3312ff6b00d88454e0b9c4d
marriola <azathoth500@gmail.com>
2015-05-25 01:42:31 -0400

    Added separate lexing and parsing tabs.

--- AST/Form1.Designer.cs
+++ AST/Form1.Designer.cs
@@ -29,15 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Program root");
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileLoad = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFileQuit = new System.Windows.Forms.ToolStripMenuItem();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Lexer = new System.Windows.Forms.TabPage();
+            this.tabParser = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.menuStrip1.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.Lexer.SuspendLayout();
+            this.tabParser.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,7 +68,7 @@
             // 
             this.menuFileLoad.Image = ((System.Drawing.Image)(resources.GetObject("menuFileLoad.Image")));
             this.menuFileLoad.Name = "menuFileLoad";
-            this.menuFileLoad.Size = new System.Drawing.Size(152, 22);
+            this.menuFileLoad.Size = new System.Drawing.Size(97, 22);
             this.menuFileLoad.Text = "&Load";
             this.menuFileLoad.Click += new System.EventHandler(this.menuFileLoad_Click);
             // 
@@ -72,33 +76,58 @@
             // 
             this.menuFileQuit.Image = ((System.Drawing.Image)(resources.GetObject("menuFileQuit.Image")));
             this.menuFileQuit.Name = "menuFileQuit";
-            this.menuFileQuit.Size = new System.Drawing.Size(152, 22);
+            this.menuFileQuit.Size = new System.Drawing.Size(97, 22);
             this.menuFileQuit.Text = "&Quit";
             this.menuFileQuit.Click += new System.EventHandler(this.menuFileQuit_Click);
             // 
-            // tableLayoutPanel1
+            // tabControl1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(292, 249);
-            this.tableLayoutPanel1.TabIndex = 1;
+            this.tabControl1.Controls.Add(this.Lexer);
+            this.tabControl1.Controls.Add(this.tabParser);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(292, 249);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Lexer
+            // 
+            this.Lexer.Controls.Add(this.listBox1);
+            this.Lexer.Location = new System.Drawing.Point(4, 22);
+            this.Lexer.Name = "Lexer";
+            this.Lexer.Padding = new System.Windows.Forms.Padding(3);
+            this.Lexer.Size = new System.Drawing.Size(284, 223);
+            this.Lexer.TabIndex = 0;
+            this.Lexer.Text = "Lexer";
+            this.Lexer.UseVisualStyleBackColor = true;
+            // 
+            // tabParser
+            // 
+            this.tabParser.Controls.Add(this.treeView1);
+            this.tabParser.Location = new System.Drawing.Point(4, 22);
+            this.tabParser.Name = "tabParser";
+            this.tabParser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabParser.Size = new System.Drawing.Size(284, 223);
+            this.tabParser.TabIndex = 1;
+            this.tabParser.Text = "Parser";
+            this.tabParser.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(278, 217);
+            this.listBox1.TabIndex = 0;
             // 
             // treeView1
             // 
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeView1.Location = new System.Drawing.Point(3, 3);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "programRoot";
-            treeNode1.Text = "Program root";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1});
-            this.treeView1.Size = new System.Drawing.Size(286, 243);
+            this.treeView1.Size = new System.Drawing.Size(278, 217);
             this.treeView1.TabIndex = 0;
             // 
             // Form1
@@ -106,13 +135,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "AST Generator";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.Lexer.ResumeLayout(false);
+            this.tabParser.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,7 +155,10 @@
         private System.Windows.Forms.ToolStripMenuItem menuFile;
         private System.Windows.Forms.ToolStripMenuItem menuFileLoad;
         private System.Windows.Forms.ToolStripMenuItem menuFileQuit;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Lexer;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabParser;
         private System.Windows.Forms.TreeView treeView1;
     }
 }

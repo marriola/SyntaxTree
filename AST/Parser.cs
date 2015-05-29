bd659d4f90b517b9ecd1be31c6e9a2e751dfd6d7
marriola <azathoth500@gmail.com>
2015-05-29 04:49:26 -0400

    Separated syntax tree definition from parser.

--- AST/Parser.cs
+++ AST/Parser.cs
@@ -6,163 +6,173 @@ using System.Threading.Tasks;
 
 namespace AST
 {
-    enum ValueType
-    {
-        INT, VOID
-    }
-
-    enum AdditiveOperator
-    {
-        PLUS, MINUS
-    }
-
-    enum MultiplicativeOperator
-    {
-        TIMES, DIVIDE
-    }
-
-    enum RelationalOperator
-    {
-        LTE, LT, EQ, NEQ, GT, GTE
-    }
-
-    enum UnaryOperator
-    {
-        INCREMENT, DECREMENT
-    }
-
-    class Node
-    {
-    }
-
-    class ProgramNode : Node
-    {
-        List<DeclarationNode> children;
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Declaration nodes
-    ///////////////////////////////////////////////////////////////////////////
-
-    class DeclarationNode : Node
-    {
-        ValueType type;
-        string name;
-    }
-
-    class ParameterNode : DeclarationNode
-    {
-        bool isArray;
-    }
-    
-    class ArrayDeclarationNode : DeclarationNode
-    {
-        int size;
-    }
-
-    class FunctionDeclarationNode : DeclarationNode
-    {
-        List<ParameterNode> parameters;
-        CompoundStatementNode body;
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Statement nodes
-    ///////////////////////////////////////////////////////////////////////////
-
-    class StatementNode : Node
-    {
-    }
-
-    class CompoundStatementNode : StatementNode
-    {
-        List<DeclarationNode> localDeclarations;
-        List<StatementNode> statements;
-    }
-
-    class IfStatementNode : StatementNode
-    {
-        ExpressionNode condition;
-        StatementNode thenStatement;
-        StatementNode elseStatement;
-    }
-
-    class ForStatementNode : StatementNode
-    {
-        ExpressionNode initializer;
-        ExpressionNode test;
-        ExpressionNode update;
-        StatementNode body;
-    }
-
-    class WhileStatementNode : StatementNode
-    {
-        ExpressionNode test;
-        StatementNode body;
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Expression nodes
-    ///////////////////////////////////////////////////////////////////////////
-
-    class ExpressionNode : Node
-    {
-        ValueType type;
-        bool isArray;
-    }
-
-    class IntegerLiteralExpressionNode : ExpressionNode
-    {
-        int value;
-    }
-
-    class VariableExpressionNode : ExpressionNode
-    {
-        string name;
-    }
-
-    class SubscriptExpressionNode : VariableExpressionNode
-    {
-        int index;
-    }
-
-    class CallExpressionNode : ExpressionNode
-    {
-        string identifier;
-        List<ExpressionNode> arguments;
-    }
-
-    class BinaryExpressionNode : ExpressionNode
-    {
-        ExpressionNode left;
-        ExpressionNode right;
-    }
-
-    class AdditiveExpressionNode : BinaryExpressionNode
-    {
-        AdditiveOperator addop;
-    }
-
-    class MultiplicativeExpressionNode : BinaryExpressionNode
-    {
-        MultiplicativeOperator multop;
-    }
-
-    class RelationalExpressionNode : BinaryExpressionNode
-    {
-        RelationalOperator relop;
-    }
-
-
-    class UnaryExpressionNode : ExpressionNode
-    {
-        VariableExpressionNode variable;
-        UnaryOperator unop;
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-
     class Parser
     {
+        private List<Token> source;
+        private int sourceIndex;
+        private Token currentToken;
+
+        private static readonly Dictionary<TokenType, ValueType> tokenToValueType = new Dictionary<TokenType, ValueType>()
+        {
+            {TokenType.INT, ValueType.INT},
+            {TokenType.VOID, ValueType.VOID}
+        };
+
+        public Parser(List<Token> source)
+        {
+            this.source = source;
+            this.currentToken = source[0];
+            this.sourceIndex = 1;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private Token NextToken()
+        {
+            return currentToken = source[sourceIndex++];
+        }
+        
+        private Token Match(TokenType tokenType)
+        {
+            if (currentToken.type != tokenType)
+            {
+                throw ParseError(tokenType.ToString());
+            }
+            return NextToken();
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private SyntaxError ParseError(string expected)
+        {
+            return new SyntaxError(currentToken.row, currentToken.column, "Expected " + expected + ", got " + currentToken.type.ToString());
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private DeclarationNode ParseVariableDeclaration(Token typeSpecifier, IdentifierToken identifier)
+        {
+            if (currentToken.type == TokenType.SEMI)
+            {
+                Match(TokenType.SEMI);
+                return new DeclarationNode(tokenToValueType[typeSpecifier.type], identifier.stringValue);
+            }
+            else if (currentToken.type == TokenType.LBRACK)
+            {
+                Match(TokenType.LBRACK);
+                IntegerToken arraySize = (IntegerToken)Match(TokenType.INTEGER);
+                Match(TokenType.RBRACK);
+
+                return new ArrayDeclarationNode(tokenToValueType[typeSpecifier.type], identifier.stringValue, arraySize.intValue);
+            }
+            else
+            {
+                throw ParseError("SEMI or LBRACK");
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private ParameterNode ParseParameter()
+        {
+            Token typeSpecifier = currentToken;
+            if (typeSpecifier.type != TokenType.INT &&
+                typeSpecifier.type != TokenType.STRING)
+            {
+                throw ParseError("INT or STRING");
+            }
+            NextToken();
+
+            IdentifierToken identifier = (IdentifierToken)currentToken;
+            Match(TokenType.ID);
+
+            bool isArray = currentToken.type == TokenType.LBRACK;
+            if (isArray)
+            {
+                Match(TokenType.LBRACK);
+                Match(TokenType.RBRACK);
+            }
+
+            return new ParameterNode(tokenToValueType[typeSpecifier.type], identifier.stringValue, isArray);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private List<ParameterNode> ParseParameterList()
+        {
+            if (currentToken.type == TokenType.VOID)
+            {
+                // void parameter list
+                Match(TokenType.VOID);
+                Match(TokenType.RPAREN);
+                return null;
+            }
+            else
+            {
+                List<ParameterNode> parameters = new List<ParameterNode>();
+                while (true)
+                {
+                    parameters.Add(ParseParameter());
+                    if (currentToken.type == TokenType.COMMA)
+                    {
+                        Match(TokenType.COMMA);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Match(TokenType.RPAREN);
+                return parameters;
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private CompoundStatementNode ParseCompoundStatement()
+        {
+            return null;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private FunctionDeclarationNode ParseFunctionDeclaration(Token typeSpecifier, IdentifierToken identifier)
+        {
+            Match(TokenType.LPAREN);
+            List<ParameterNode> parameters = ParseParameterList();
+            Match(TokenType.RPAREN);
+            CompoundStatementNode body = ParseCompoundStatement();
+            return new FunctionDeclarationNode(tokenToValueType[typeSpecifier.type], identifier.stringValue, parameters, body);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public ProgramNode Parse()
+        {
+            ProgramNode program = new ProgramNode();
+            while (sourceIndex < source.Count)
+            {
+                if (currentToken.type != TokenType.INT && currentToken.type != TokenType.VOID)
+                {
+                    throw ParseError("INT or VOID");
+                }
+                Token typeSpecifier = currentToken;
+                Match(typeSpecifier.type);
+
+                Match(TokenType.ID);
+                IdentifierToken identifier = (IdentifierToken)currentToken;
+
+                if (currentToken.type == TokenType.SEMI || currentToken.type == TokenType.LBRACK)
+                {
+                    program.children.Add(ParseVariableDeclaration(typeSpecifier, identifier));
+                }
+                else
+                {
+                    program.children.Add(ParseFunctionDeclaration(typeSpecifier, identifier));
+                }
+            }
+
+            return null;
+        }
     }
 }
---
+++ AST/SyntaxTree.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST
+{
+    enum ValueType
+    {
+        INT, VOID
+    }
+
+    enum AdditiveOperator
+    {
+        PLUS, MINUS
+    }
+
+    enum MultiplicativeOperator
+    {
+        TIMES, DIVIDE
+    }
+
+    enum RelationalOperator
+    {
+        LTE, LT, EQ, NEQ, GT, GTE
+    }
+
+    enum UnaryOperator
+    {
+        INCREMENT, DECREMENT
+    }
+
+    class Node
+    {
+    }
+
+    class ProgramNode : Node
+    {
+        public List<DeclarationNode> children;
+
+        public ProgramNode()
+        {
+            children = new List<DeclarationNode>();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Declaration nodes
+    ///////////////////////////////////////////////////////////////////////////
+
+    class DeclarationNode : Node
+    {
+        ValueType type;
+        string identifier;
+
+        public DeclarationNode(ValueType type, string identifier)
+        {
+            this.type = type;
+            this.identifier = identifier;
+        }
+    }
+
+    class ParameterNode : DeclarationNode
+    {
+        bool isArray;
+
+        public ParameterNode(ValueType type, string identifier, bool isArray)
+            : base(type, identifier)
+        {
+            this.isArray = isArray;
+        }
+    }
+
+    class ArrayDeclarationNode : DeclarationNode
+    {
+        int size;
+
+        public ArrayDeclarationNode(ValueType type, string identifier, int size)
+            : base(type, identifier)
+        {
+            this.size = size;
+        }
+    }
+
+    class FunctionDeclarationNode : DeclarationNode
+    {
+        List<ParameterNode> parameters;
+        CompoundStatementNode body;
+
+        public FunctionDeclarationNode(ValueType type, string identifier, List<ParameterNode> parameters, CompoundStatementNode body)
+            : base(type, identifier)
+        {
+            this.parameters = parameters;
+            this.body = body;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Statement nodes
+    ///////////////////////////////////////////////////////////////////////////
+
+    class StatementNode : Node
+    {
+    }
+
+    class CompoundStatementNode : StatementNode
+    {
+        List<DeclarationNode> localDeclarations;
+        List<StatementNode> statements;
+    }
+
+    class IfStatementNode : StatementNode
+    {
+        ExpressionNode condition;
+        StatementNode thenStatement;
+        StatementNode elseStatement;
+    }
+
+    class ForStatementNode : StatementNode
+    {
+        ExpressionNode initializer;
+        ExpressionNode test;
+        ExpressionNode update;
+        StatementNode body;
+    }
+
+    class WhileStatementNode : StatementNode
+    {
+        ExpressionNode test;
+        StatementNode body;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Expression nodes
+    ///////////////////////////////////////////////////////////////////////////
+
+    class ExpressionNode : Node
+    {
+        ValueType type;
+        bool isArray;
+    }
+
+    class IntegerLiteralExpressionNode : ExpressionNode
+    {
+        int value;
+    }
+
+    class VariableExpressionNode : ExpressionNode
+    {
+        string name;
+    }
+
+    class SubscriptExpressionNode : VariableExpressionNode
+    {
+        int index;
+    }
+
+    class CallExpressionNode : ExpressionNode
+    {
+        string identifier;
+        List<ExpressionNode> arguments;
+    }
+
+    class BinaryExpressionNode : ExpressionNode
+    {
+        ExpressionNode left;
+        ExpressionNode right;
+    }
+
+    class AdditiveExpressionNode : BinaryExpressionNode
+    {
+        AdditiveOperator addop;
+    }
+
+    class MultiplicativeExpressionNode : BinaryExpressionNode
+    {
+        MultiplicativeOperator multop;
+    }
+
+    class RelationalExpressionNode : BinaryExpressionNode
+    {
+        RelationalOperator relop;
+    }
+
+
+    class UnaryExpressionNode : ExpressionNode
+    {
+        VariableExpressionNode variable;
+        UnaryOperator unop;
+    }
+}
